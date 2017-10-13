46d1541f224b4ba05a924531e2fdd80cbd5353fd
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2017-10-13 09:52:33 +0100

    add riffraff yaml

--- src/main/scala/com/gu/microserviceWorkshop/Lambda.scala
+++ src/main/scala/com/gu/microserviceWorkshop/Lambda.scala
@@ -1,13 +1,6 @@
 package com.gu.microserviceWorkshop
-import java.io.{InputStream, OutputStream}
 
 import com.typesafe.scalalogging.LazyLogging
-import io.circe.parser._
-import io.circe.syntax._
-import java.nio.charset.StandardCharsets.UTF_8
-import cats.syntax.either._
-
-
 
 object Lambda extends LazyLogging {
 
